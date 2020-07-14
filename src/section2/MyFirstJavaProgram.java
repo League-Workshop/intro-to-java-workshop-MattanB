2f3cbc2699b901b980d40e00a38d6db7c4f2a4ae
MattanB <68243625+MattanB@users.noreply.github.com>
2020-07-14 19:02:50 +0000

    Coding Camp Day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,21 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+		int angle = 90;
+		int distance = 100;
 		
+		Robot rob = new Robot();
+		
+		rob.setSpeed(100);
+		
+		rob.penDown();
+		
+		for (int i = 0 ; i < 4; i++) {
+		
+		rob.move(distance);
+		
+		rob.turn(angle);
+		}
 		
 	}
 }
--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -6,24 +6,30 @@ public class SmartShapes {
 	
     public static void main(String[] args) throws Exception {
         // 1. Make a new Robot
-
-
+    	Robot mattan = new Robot();
+    	int distance = 250;
+    	int angle = 90;
+    	int side = 360;
         // 3. Put the robot's pen down
-
+    	mattan.penDown();
 
         // 6. Make the robot move as fast as possible
-
+    	mattan.setSpeed(200);
 
         // 5. Use a for loop to repeat everything below 4 times. 
-
-
+    	for (int i = 0 ; i < side; i++) {
+    		mattan.setRandomPenColor();
         //         2. Move your robot 200 pixels
-
+    		mattan.move(distance);
 
         //         4. Turn the robot 90 degrees to the right
-
+    		mattan.turn(angle);
+    		
+    		mattan.turn(1);
     	
     		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
-   
+    	
+    	
+    	}
     }
 }
