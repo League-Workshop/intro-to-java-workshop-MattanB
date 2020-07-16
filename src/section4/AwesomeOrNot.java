df57da9aae852f85b64e4a85c7ddfa21e0f298aa
root <root@7e6671743fe6>
2020-07-16 18:43:25 +0000

    Day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,10 +2,29 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+public static void main(String[] args) {
+	Random randy = new Random();
+	
+	int randomNumber = randy.nextInt(4);
+	JOptionPane.showInputDialog("What's something you think is awesome?");
+	if(randomNumber == 0) {
+		JOptionPane.showMessageDialog(null, "That's awesome!");
+	}
+	if(randomNumber == 1) {
+		JOptionPane.showMessageDialog(null, "Ehh... That's ok");
+	}
+	if(randomNumber == 2) {
+		JOptionPane.showMessageDialog(null, "BORING");
+	}
+	if(randomNumber == 3) {
+		JOptionPane.showMessageDialog(null, "I don't think you and I have similar definitions of awesome \n" + "because that is as not awesome as it gets...");
+	}
+} 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
 	// 3. Print out this variable
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -13,7 +13,7 @@ public class QuizGame {
 		// 3.  Use an if statement to check if their answer is correct
 			if(q1.equalsIgnoreCase("2")) {
 				score ++;
-				JOptionPane.showMessageDialog(null, "Correct!");
+				JOptionPane.showMessageDialog(null, "Bingo!");
 			}
 			else {
 				JOptionPane.showMessageDialog(null, "Sorry, but the answer was 2! You'll get it next time!");
@@ -23,7 +23,7 @@ public class QuizGame {
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 			if(q2.equalsIgnoreCase("9.58")) {
 				score ++;
-				JOptionPane.showMessageDialog(null, "Correct!");
+				JOptionPane.showMessageDialog(null, "Spot on!");
 			}
 			else {
 				JOptionPane.showMessageDialog(null, "The correct answer was 9.58!");
@@ -37,14 +37,54 @@ public class QuizGame {
 			else {
 				JOptionPane.showMessageDialog(null, "The world's largest animal is the Blue Whale! Not " + q3 + ".");
 			}
-			String q4 = JOptionPane.showInputDialog("Who made this quiz?");
-			if(q4.equalsIgnoreCase("Mattan")) {
+			String q4 = JOptionPane.showInputDialog("Who invented the bottle cap?");
+			if(q4.equalsIgnoreCase("William Painter")) {
 				score ++;
-				JOptionPane.showMessageDialog(null, "Correct!");
+				JOptionPane.showMessageDialog(null, "You got it!");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "The bottle cap was invented by William Painter.");
+			}
+			String q5 = JOptionPane.showInputDialog("Who has the most grand slams titles in men's tennis as of 2020?");
+			if(q5.equalsIgnoreCase("Roger Federer")) {
+				score ++;
+				JOptionPane.showMessageDialog(null, "100%!");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Nope. It's Roger Federer");
+			}
+			String q6 = JOptionPane.showInputDialog("How much wood would a woodchuck chuck if a woodchuck would chuck wood?");
+			if(q6.equalsIgnoreCase("700 pounds worth of wood")) {
+				score ++;
+				JOptionPane.showMessageDialog(null, "Splendid work!");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Sorry, but the answer was 700 pounds because they can chuck 700 pounds worth of dirt so \n" + "they can chuck an equivalent amount of wood");
+			}
+			String q7 = JOptionPane.showInputDialog("what is 1*1?");
+			if(q7.equalsIgnoreCase("1")) {
+				score ++;
+				JOptionPane.showMessageDialog(null, "Yep, you got it");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "WRONG! U = Dumb >:c");
+			}
+			String q8 = JOptionPane.showInputDialog("who won the 2020 superbowl?");
+			if(q8.equalsIgnoreCase("The Chiefs")) {
+				score ++;
+				JOptionPane.showMessageDialog(null, "Mhm");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Wrong answer.");
+			}
+			String q9 = JOptionPane.showInputDialog("Who made this quiz?");
+			if(q9.equalsIgnoreCase("Mattan")) {
+				score ++;
+				JOptionPane.showMessageDialog(null, "Yessir!");
 			}
 			else {
 				JOptionPane.showMessageDialog(null, "Mattan made this quiz.");
 			}
-			JOptionPane.showMessageDialog(null, "You got " + score + "/4 questions correct! Thank you for playing!");
+			JOptionPane.showMessageDialog(null, "You got " + score + "/8 questions correct! Thank you for playing!");
 	}
 }
